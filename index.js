9ce4546c7fbc9ca9d67c9dd62e48149b360bcf74
Zachary <zvcoding@gmail.com>
2022-01-24 13:25:49 -0500

    commit tree 68cdc34fbcd93decc2d61d9f967c2641f24a5a74

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Your code here
+
+class Polygon{
+  constructor(arrayZ){
+    this.arrayZ = arrayZ
+  }
+
+  get countSides(){
+    let numberOfsides = this.arrayZ.length 
+    return numberOfsides
+  }
+
+  get perimeter(){
+    let sum =this.arrayZ.reduce(function (previousValue, currentValue) {
+      return previousValue + currentValue
+    }, 0)
+    return sum
+  }
+}
+
+class Triangle extends Polygon{
+  get isValid(){
+    if(this.countSides === 3){
+        if(this.arrayZ[0] + this.arrayZ[1]<this.arrayZ[2]){
+         return false
+        }
+
+        if(this.arrayZ[1] + this.arrayZ[2]<this.arrayZ[0]){
+          return false
+        }
+
+        if(this.arrayZ[2] + this.arrayZ[0]<this.arrayZ[1]){
+          return false
+        }
+
+        return true
+        
+      }
+  
+  } 
+  }
+
+  class Square extends Polygon{
+    get isValid(){
+      if(this.countSides !=4){
+        return false
+      } 
+      if(this.arrayZ[0]===this.arrayZ[1]&&this.arrayZ[0]===this.arrayZ[2]&&this.arrayZ[0]===this.arrayZ[3]){
+        return true
+      }else{
+        return false
+      }
+      }
+    
+    get area(){
+      return this.arrayZ[0] * this.arrayZ[1]
+    }
+  }
+
+let test1 = [1,1,1]
+
+let newShape = new Polygon(test1)
+let newTriangle = new Triangle(test1)
+console.log(newTriangle.isValid)
